6d88bcd4563951b4dd40572198ad054b668f936a
aaron <1491157+aar0u@users.noreply.github.com>
2025-11-14 19:25:48 +0800

    refactor(routes): update route definitions for improved handling

--- proxy/src/server.ts
+++ proxy/src/server.ts
@@ -10,15 +10,14 @@ import { urlController } from "./controllers/urlController";
 import { getOrigin } from "./utils";
 
 const routes = {
-  "/static/": fileController("../../static").handle,
-  "/js/": fileController("../../../js").handle,
-  "/lib/": fileController("../../../lib").handle,
-  "/json/": fileController("../../../json").handle,
-  "/url/": urlController.handle,
-  "/sub/fetch": subController.fetch,
-  "/sub/": subController.handle,
-  "/json": jsonController.handle,
-  "/": jsonController.handle,
+   "/js/*": fileController("../../../js").handle,
+   "/lib/*": fileController("../../../lib").handle,
+   "/json/*": fileController("../../../json").handle,
+   "/url/*": urlController.handle,
+   "/sub/fetch*": subController.fetch,
+   "/sub/*": subController.handle,
+   "/": jsonController.handle,
+  "/*": fileController("../../static").handle,
 };
 
 const server = createServer(async (req, res) => {
@@ -30,9 +29,16 @@ const server = createServer(async (req, res) => {
 
   const route = new URL(req.url, getOrigin(req)).pathname;
 
-  const handler = Object.entries(routes).find(
-    ([path]) => route.startsWith(path) || route === path,
-  )?.[1];
+  let handler = routes[route];
+  if (!handler) {
+    handler = Object.entries(routes).find(([path]) => {
+      if (path.endsWith("*")) {
+        const base = path.slice(0, -1);
+        return route.startsWith(base);
+      }
+      return false;
+    })?.[1];
+  }
 
   if (handler) {
     await handler(req, res);
